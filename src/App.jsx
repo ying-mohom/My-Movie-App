f43594b50fb1acaf8d47ce671e0ea1e5fc23daca
bcmm.intern12 <yingmohom64@gmail.com>
2023-08-28 15:27:36 +0630

    initial commit for movie app

---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+
+import { useState, useEffect } from 'react';
+import { fetchDataFromApi } from './utils/api';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
+
+import { UseSelector, useDispatch, useSelector } from 'react-redux';
+import { getApiConfiguration } from './store/homeSlice';
+
+
+
+import Header from "./components/header/Header";
+import Footer from './components/footer/Footer';
+import Home from './pages/home/Home';
+import Details from './pages/details/Details';
+import SearchResult from './pages/searchResult/SearchResult';
+import Explore from './pages/explore/Explore';
+import pageNotFound from './pages/404/pageNotFound';
+
+
+
+const App = () => {
+    const dispatch = useDispatch();
+
+    const url = useSelector((store) =>
+        store.home);
+    console.log(url);
+
+    useEffect(() => {
+        fetchApiConfig();
+    }, [])
+
+    const fetchApiConfig = () => {
+        fetchDataFromApi('/configuration')
+            .then((res) => {
+                console.log(res);
+
+                const url = {
+                    backdrop: res.images.secure_base_url +
+                        "original",
+                    poster: res.images.secure_base_url +
+                        "original",
+                    profile: res.images.secure_base_url +
+                        "original",
+                }
+                dispatch(getApiConfiguration(url))
+            })
+    }
+    return (
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/:mediaType/:id" element={<Details />} />
+                <Route path="/search/:query" element={<SearchResult />} />
+                <Route path="/explore/:mediaType" element={<Explore />} />
+                <Route path="*" element={<pageNotFound />} />
+            </Routes>
+            <Footer />
+        </BrowserRouter>
+    )
+}
+
+export default App;
---
+++ src/utils/api.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const BASE_URL = "https://api.themoviedb.org/3";
+const TMDB_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMjkxZWU3MjcxMjczNzhhNmY4NjY3N2QyMjBlN2Q0ZSIsInN1YiI6IjY0ZWMxOGM2YzNjODkxMDBlMzYxM2EwZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.XpAyJjak5hjOtsajDc8UyVvSe7orsMSZYFOFXTzP_LE";
+
+
+const headers = {
+    Authorization: "bearer " + TMDB_TOKEN,
+};
+
+export const fetchDataFromApi = async (url, params) => {
+    try {
+        const { data } = await axios.get(BASE_URL + url, {
+            headers,
+            params
+        })
+        return data;
+    } catch (err) {
+        console.log(err);
+        return err;
+    }
+}
